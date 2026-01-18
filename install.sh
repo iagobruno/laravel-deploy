d4dae4fdd73480b1e24c7733a61f01958f7fd09f
Iago Bruno <iagobruno.dev@gmail.com>
2026-01-18 03:40:47 -0300

    .

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ CRON_CMD="* * * * * /usr/bin/php $(pwd)/artisan schedule:run >> /dev/null 2>&1";
 crontab -l 2>/dev/null | grep -F "$CRON_CMD" >/dev/null || \
   (crontab -l 2>/dev/null; echo "$CRON_CMD") | crontab -;
 
-chown -R www-data:www-data storage bootstrap/cache && chmod -R 775 storage bootstrap/cache;
+chmod -R 777 storage bootstrap/cache;
 mkdir -p /var/log/supervisor && chown -R root:root /var/log/supervisor;
 
 systemctl enable supervisor && systemctl start supervisor;
