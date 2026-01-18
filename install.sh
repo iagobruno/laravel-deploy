e54e40cfed284993577a3859181d3ffe4fa74ad3
Iago Bruno <iagobruno.dev@gmail.com>
2026-01-18 02:53:08 -0300

    .

--- install.sh
+++ install.sh
@@ -34,8 +34,6 @@ apt install -y php8.4 \
     php8.4-intl \
     php8.4-common \
     php8.4-bcmath \
-    php8.4-ctype \
-    php8.4-fileinfo \
     php8.4-mbstring \
     php8.4-opcache \
     php8.4-imagick \
@@ -44,7 +42,6 @@ apt install -y php8.4 \
     php8.4-mysql \
     php8.4-pgsql \
     php8.4-redis \
-    php8.4-tokenizer \
     php8.4-curl \
     php8.4-xml \
     php8.4-zip;
